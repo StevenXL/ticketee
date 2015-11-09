f267267f23e24e58779a80e2fba65228c2a4de88
Steven Leiva <leiva.steven@gmail.com>
2015-11-09 09:52:10 -0500

    Redirect user to the projects page if the project they are trying to view does not exist.
    Note that, unlike the Rails 4 in Action tutorial, I am using `find_by`
    instead of `find`. `find_by` does not raise an error / exception if it
    cannot find the record. This means that the set_project method is
    slightly different. However, the tests are the same, and they still
    pass.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -20,16 +22,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find_by(id: params[:id])
   end
 
   def edit
-    @project = Project.find_by(id: params[:id])
   end
 
   def update
-    @project= Project.find_by(id: params[:id])
-
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -40,13 +38,21 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find_by(id: params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
 
   private
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
+
+    if @project.nil?
+      flash[:alert] = "The project you were looking for cannot be found."
+      redirect_to projects_path
+    end
+  end
 
   def project_params
     params.require(:project).permit(:name, :descripton)
