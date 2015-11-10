d3c270fc505c0324684981c83457f176eb8be7dd
Steven Leiva <leiva.steven@gmail.com>
2015-11-10 15:36:54 -0500

    Handle authorization errors gracefully.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+rescue_from Pundit::NotAuthorizedError, with: :not_authorized
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def not_authorized
+    redirect_to root_path, alert: "You aren't allowed to do that."
+  end
 end
